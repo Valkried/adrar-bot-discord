ecc50027e00a993035721d44d18fc885784b3a50
Valkried <celebow@gmail.com>
2020-07-05 17:41:14 +0200

    First commit

---
+++ class/AdelBot.js
@@ -0,0 +1,117 @@
+class AdelBot {
+
+    constructor() {
+        this.chooseNewSentence();
+    }
+
+    params = {
+        /* Prod params : */
+        // channelId: '720298334522179700',
+        // roleId: '720298334522179698',
+        /* Test params : */
+        channelId: '724964399051309138',
+        roleId: '725981703222460457',
+        morningTime: { hour: 8, minute: 55 },
+        afternoonTime: { hour: 18, minute: 0 },
+        reminderTime: 41, // in minutes
+        url: 'https://adel.adrar-formation.eu/login/index.php',
+        sentences: [
+            'Hey ! Il est l\'heure de se connecter sur ADEL =)',
+            'Debout là-dedans ! On se connecte à ADEL',
+            'ADEL needs YOU',
+            'Hello... It\'s me...',
+            'Venez vite, j\'ai des cookies !'
+        ]
+    };
+
+    message;
+    alertMessage;
+    programStarted = true;
+    targetDateStart;
+    targetDateEnd;
+
+    /**
+     * Set the date of the next alert for the morning, the afternoon, or the next morning day.
+     */
+    setTargetDate() {
+        let actualDate = new Date();
+        /* Tests : */
+        // let actualDate = new Date('2020-07-06 14:00:00'); // Monday
+        // let actualDate = new Date('2020-07-07 08:00:00'); // Tuesday
+        // let actualDate = new Date('2020-07-08 08:00:00'); // Wednesday
+        // let actualDate = new Date('2020-07-09 08:00:00'); // Thursday
+        // let actualDate = new Date('2020-07-10 08:00:00'); // Friday
+        // let actualDate = new Date('2020-07-11 08:00:00'); // Saturday
+        // let actualDate = new Date('2020-07-12 08:00:00'); // Sunday
+        this.targetDateStart = new Date(actualDate);
+
+        // Morning time this day
+        this.targetDateStart.setHours(this.params.morningTime.hour);
+        this.targetDateStart.setMinutes(this.params.morningTime.minute);
+        this.targetDateStart.setSeconds(0);
+
+        this.setTargetDateEnd();
+
+        // If the actual time is > morning time OR if it is saturday or sunday
+        if (actualDate >= this.targetDateEnd || [6, 0].includes(actualDate.getDay())) {
+
+            // Afternoon time this day
+            this.targetDateStart.setHours(this.params.afternoonTime.hour);
+            this.targetDateStart.setMinutes(this.params.afternoonTime.minute);
+
+            // If it is saturday : + 2 days | If it is sunday : + 1 day
+            switch (this.targetDateStart.getDay()) {
+                case 6: // Saturday
+                    this.targetDateStart.setHours(this.targetDateStart.getHours() + 48);
+                    break;
+                case 0: // Sunday
+                    this.targetDateStart.setHours(this.targetDateStart.getHours() + 24);
+            }
+
+            this.setTargetDateEnd();
+
+            // If the actual time is > afternoon time this day, or if it is saturday or sunday
+            if (actualDate >= this.targetDateEnd || [6, 0].includes(actualDate.getDay())) {
+                this.targetDateStart.setHours(this.params.morningTime.hour);
+                this.targetDateStart.setMinutes(this.params.morningTime.minute);
+
+                if (actualDate.getDay() === 5) { // Friday
+                    this.targetDateStart.setHours(this.targetDateStart.getHours() + 72);
+                } else if (actualDate.getDay() > 0 && actualDate.getDay() < 5) { // Monday to thursday
+                    this.targetDateStart.setHours(this.targetDateStart.getHours() + 24);
+                }
+
+                this.setTargetDateEnd();
+            }
+        }
+
+    }
+
+    /**
+     * Sets the target date end depending on the target date start and the reminder time.
+     */
+    setTargetDateEnd() {
+        this.targetDateEnd = new Date(this.targetDateStart.getTime());
+        this.targetDateEnd.setMinutes(this.targetDateEnd.getMinutes() + this.params.reminderTime);
+    }
+
+    /**
+     * Chooses a random sentence and makes a message to display on the Discord channel.
+     */
+    chooseNewSentence() {
+        let sentence = this.params.sentences[Math.floor(Math.random() * this.params.sentences.length)];
+        this.message = `<@&${this.params.roleId}> ${sentence} <${this.params.url}>`;
+    }
+
+    /**
+     * Logs an array of string events with date and time value, and adds a separator after that.
+     */
+    logEvents(events) {
+        events.forEach(event => {
+            console.log(new Date().toLocaleString() + ' - ' + event);
+        });
+        console.log('-----');
+    }
+}
+
+module.exports = AdelBot;
---
+++ index.js
@@ -0,0 +1,199 @@
+/*
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', msg => {
+    sendMessageEachDay();
+});
+
+/!**
+ * From monday to friday, morning and afternoon, a message will be sent each day to all users on a specific channel, to reminds them to log in ADEL.
+ * The message will be automatically deleted after 15 minutes.
+ * You can change the parameters to match your configuration, and specify the channel, the hours, the url to log in and some sentences as funny reminders.
+ * To find the channel's ID, just mention it in Discord with a backslash in front of the mention.
+ *!/
+function sendMessageEachDay() {
+    const params = {
+        channelId: '720298334522179700', // test : 724964399051309138 | adrar : 720298334522179700
+        roleId: '720298334522179698', // test : 725981703222460457 | adrar : 720298334522179698
+        morningTime: { hour: 8, minute: 55 },
+        afternoonTime: { hour: 13, minute: 25 },
+        url: 'https://adel.adrar-formation.eu/login/index.php',
+        sentences: [
+            'Hey ! Il est l\'heure de se connecter sur ADEL =)',
+            'Debout là-dedans ! On se connecte à ADEL',
+            'ADEL needs YOU',
+            'Hello... It\'s me...',
+            'Venez vite, j\'ai des cookies !'
+        ]
+    };
+
+    const channel = client.channels.cache.get(params.channelId);
+
+    if (channel !== undefined) {
+        setInterval(() => {
+            const sentence = params.sentences[Math.floor(Math.random() * params.sentences.length)];
+            const date = new Date();
+            const day = date.getDay(),
+                  hour = date.getHours(),
+                  minute = date.getMinutes();
+
+            if (day >= 1 && day <= 5) {
+                if ((hour === params.morningTime.hour && minute === params.morningTime.minute) || (hour === params.afternoonTime.hour && minute === params.afternoonTime.minute)) {
+                    channel.send(`<@&${params.roleId}> ${sentence} <${params.url}>`).then(msg => {
+                        msg.delete({timeout: 300000});
+                    });
+                }
+            }
+        }, 60000);
+    }
+}
+
+client.login('NzI0OTYyNzIzNDAxNTY0MjIx.XvH0xQ.PSRo-DUincURwBJpTi_pxmxGRao');
+*/
+
+
+
+
+
+
+/**
+ * Tests
+ */
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const firebase = require('firebase/app');
+require('firebase/database');
+const firebaseConfig = {
+    // apiKey: "AIzaSyDNL8qKBFRF3DL_Pgi6tVuUTkRo9stBcSA",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "adel-bot.firebaseapp.com",
+    databaseURL: "https://adel-bot.firebaseio.com",
+    projectId: "adel-bot",
+    storageBucket: "adel-bot.appspot.com",
+    messagingSenderId: "904980287998",
+    appId: "1:904980287998:web:c7ca4f0e5d2e12872db0af"
+};
+firebase.initializeApp(firebaseConfig);
+
+const AdelBot = require('./class/AdelBot');
+const adelBot = new AdelBot();
+
+client.on('ready', () => {
+    sendMessageEachDay();
+});
+
+/**
+ * From monday to friday, morning and afternoon, a message will be sent each day to all users on a specific channel, to reminds them to log in ADEL.
+ * The message will be automatically deleted after 15 minutes.
+ * You can change the parameters to match your configuration, and specify the channel, the hours, the url to log in and some sentences as funny reminders.
+ * To find the channel's ID, just mention it in Discord with a backslash in front of the mention.
+ */
+function sendMessageEachDay() {
+    const channel = client.channels.cache.get(adelBot.params.channelId);
+
+    console.log('****************');
+    console.log('Starting program');
+    console.log('****************');
+
+    adelBot.setTargetDate();
+
+    adelBot.logEvents([
+        'The next alert will start at : ' + adelBot.targetDateStart.toLocaleString(),
+        'The next alert will end at : ' + adelBot.targetDateEnd.toLocaleString()
+    ]);
+
+    if (channel !== undefined && ![6, 0].includes(adelBot.targetDateStart.getDay())) {
+            // Si le programme a été redémarré pendant la date de début et la date de fin
+        if ((Date.now() >= adelBot.targetDateStart && Date.now() < adelBot.targetDateEnd) && adelBot.programStarted) {
+            firebase.database().ref('alert-message').on('value', (data) => {
+                if (adelBot.programStarted) {
+                    adelBot.programStarted = false;
+                    adelBot.logEvents(['The program has been rebooted.']);
+                    adelBot.alertMessage = data.val();
+
+                    if (adelBot.alertMessage.messageSent === true) { // Si le message a été envoyé
+                        adelBot.logEvents(['A message is still there.']);
+                        setTimeout(() => {
+                            channel.messages.fetch(adelBot.alertMessage.messageId).then(msg => {
+                                msg.delete().catch(reason => {
+                                    console.log(reason);
+                                });
+                            });
+                            deleteMessage();
+                        }, adelBot.targetDateEnd - Date.now());
+                    } else { // Si le message n'a pas été envoyé
+                        adelBot.logEvents(['No message has been found.'])
+                        channel.send(adelBot.message).then(msg => {
+                            addMessageToDatabase(msg);
+
+                            adelBot.setTargetDate();
+
+                            setTimeout(() => {
+                                deleteMessage(msg);
+                            }, adelBot.targetDateEnd - Date.now());
+                        });
+                    }
+                }
+            });
+        } else {
+            adelBot.programStarted = false;
+
+            adelBot.logEvents([
+                'The next alert will be sent in ' + Math.round((new Date(adelBot.targetDateStart - Date.now()).getTime() / 1000)) + ' seconds.'
+            ]);
+
+            setTimeout(() => {
+                channel.send(adelBot.message).then(msg => {
+                    addMessageToDatabase(msg);
+
+                    adelBot.logEvents([
+                        'The message will be deleted in ' + Math.round((new Date(adelBot.targetDateEnd - Date.now()).getTime() / 1000)) + ' seconds.'
+                    ]);
+
+                    setTimeout(() => {
+                        deleteMessage(msg);
+                    }, adelBot.targetDateEnd - Date.now());
+                });
+            }, adelBot.targetDateStart - Date.now());
+        }
+    }
+}
+
+/**
+ * Add a message to Firebase
+ */
+function addMessageToDatabase(msg) {
+    firebase.database().ref('alert-message').set({
+        messageSent: true,
+        messageId: msg.id
+    }).then(() => {
+        adelBot.logEvents(['Message posted']);
+    });
+}
+
+/**
+ * Deletes a message from Firebase
+ */
+function deleteMessage(msg = undefined) {
+    if (msg !== undefined) {
+        msg.delete();
+    }
+
+    firebase.database().ref('alert-message').set({
+        messageSent: false,
+        messageId: ''
+    }).then(() => {
+        adelBot.logEvents(['Message deleted']);
+    });
+
+    sendMessageEachDay();
+}
+
+// client.login('NzI0OTYyNzIzNDAxNTY0MjIx.XvH0xQ.PSRo-DUincURwBJpTi_pxmxGRao');
+client.login(process.env.BOT_TOKEN);
+
+// url API jours fériés : https://calendrier.api.gouv.fr/jours-feries/metropole.json
+
